f3c8403a8698b07735eb6c867fe14e20dd693b79
Ajender <ajender@famcominc.com>
2023-06-13 22:39:32 +0530

    recharge
    recharge

--- src/app/accounts/pages/account-recharge/account-recharge.component.ts
+++ src/app/accounts/pages/account-recharge/account-recharge.component.ts
@@ -1,20 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { CurrentSetting } from 'app/core/models/current-setting';
 import { SearchRatesService } from 'app/rates/searchrates.service';
-
+import { Plan } from '../../models/plan';
 @Component({
   selector: 'app-account-recharge',
   templateUrl: './account-recharge.component.html',
   styleUrls: ['./account-recharge.component.scss']
 })
 export class AccountRechargeComponent implements OnInit {
-
+  @Input() plan: Plan;
   currentSetting: CurrentSetting;
+  callingFrom : any;
+  callingTo : any;
+
   constructor(
     private searchRatesService: SearchRatesService,
   ) { }
 
   ngOnInit(): void {
+
+    this.callingFrom = this.plan.CountryFrom;
+    this.callingTo = this.plan.CountryTo;
+
     this.getRates();
   }
   
@@ -22,7 +29,7 @@ export class AccountRechargeComponent implements OnInit {
    
     getRates()
     {
-      // this.searchRatesService.getSearchGlobalRates(this.currentSetting.currentCountryId, this.countryId).subscribe(
-      //   (data: any) => {})
+      this.searchRatesService.getSearchGlobalRates(this.callingFrom, this.callingTo).subscribe(
+        (data: any) => {  })
    }
 }
