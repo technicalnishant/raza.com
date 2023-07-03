c0699d9e4c4fd71f46bbef909442e89bda0c3829
Ajender <ajender@famcominc.com>
2023-07-03 20:02:09 +0530

    account recharge confirmation box

---
+++ src/app/accounts/pages/recharge-confirmation/recharge-confirmation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recharge-confirmation',
+  templateUrl: './recharge-confirmation.component.html',
+  styleUrls: ['./recharge-confirmation.component.scss']
+})
+export class RechargeConfirmationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
