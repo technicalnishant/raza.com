125f7c6168aab9a88c6aafca19954d7010d0a973
Ajender <ajender@famcominc.com>
2023-08-29 22:13:21 +0530

    class to hide nav

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -78,6 +78,7 @@ export class HeaderComponent implements AfterViewInit, OnInit  {
   filter_string:string=''
   ctryName:any;
   previousUrl:any ='';
+  navClick:any=''
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -604,9 +605,12 @@ onClickClose(icon)
   
   clickMenu()
   {
-    let value = (this.razaLayoutService.getSharedValue())?'':'hide_nav';
-    this.razaLayoutService.setSharedValue(value);
-    console.log(value);
+    
+    
+    this.navClick = (this.navClick == '')?'hide_nav':'';
+    
+    this.razaLayoutService.setSharedValue(this.navClick);
+   
   }
 
   
