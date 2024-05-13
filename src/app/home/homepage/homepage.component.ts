168077bd09db0d3fa21d4606c34c129e57530a21
Ajender <ajender@famcominc.com>
2024-05-13 19:35:05 +0530

    build without promotion

--- src/app/home/homepage/homepage.component.ts
+++ src/app/home/homepage/homepage.component.ts
@@ -97,7 +97,7 @@ sliderAutoplay: boolean = true;
   contentLoaded: boolean = false;
   isFixHeader: boolean = false;
    
-   promtionCode:string='MOM2024';
+   promtionCode:string='';
   currentURL:any;
   defaultImage = '';
   previousUrl:any='';
